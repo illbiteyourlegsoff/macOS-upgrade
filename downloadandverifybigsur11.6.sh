6433d562056dfd0ece68bbb389ee6883eedb6b77
Trey Howell <42068057+illbiteyourlegsoff@users.noreply.github.com>
2021-09-20 07:24:52 -0500

    Create downloadandverifybigsur11.6.sh

---
+++ downloadandverifybigsur11.6.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+cd /tmp
+
+curl -O http://swcdn.apple.com/content/downloads/57/38/071-97382-A_OEKYSXCO6D/97vrhncortwd3i38zfogcscagmpwksdzce/InstallAssistant.pkg
+
+
+
+### 256 chksum 11.5.2
+chksm=6142d4200f415d1253c437bdfcc7911bfb568ed16de1f0350bbc76299940647a
+echo $chksm
+
+verchk=`openssl dgst -sha256 /tmp/InstallAssistant.pkg | awk '{print $NF}'`
+echo $verchk 
+
+if [ $chksm == $verchk ]
+then
+cd /tmp
+
+#curl -O http://swcdn.apple.com/content/downloads/43/16/071-78704-A_U5B3K7DQY9/cj9xbdobsdoe67yq9e1w2x0cafwjk8ofkr/InstallAssistant.pkg
+
+installer -pkg /tmp/InstallAssistant.pkg -target /
+
+else
+echo "not fully downloaded or invalid"
+exit 0
+fi
